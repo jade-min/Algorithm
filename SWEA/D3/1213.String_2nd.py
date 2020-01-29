a619c6423a749957e331d3eb70771e4fcb2aa5eb
jade-min <smp1991@naver.com>
2020-01-29 10:50:57 +0900

    알고리즘 : SWEA D3 Python 1215번
    * 반복문과 조건문 그리고 Slicing
    - 반복문 돌릴 때 range 크게 잡으면 좋겠지만 딱 필요한 만큼만 돌리면 메모리와 시간 모두 절약할 수 있음
    - 조건문 분기가 여러 개 들어가더라도 그게 하나로 합쳐질 수 있다면 합치는 게 좋겠지
    - [:] <= 이 Slicing은 변수에 연산이 한 번이 들어간 것이다

---
+++ SWEA/D3/1213.String_2nd.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("1213.String_input.txt", "rt", encoding="UTF-8")
+
+T = 10
+
+for tc in range(1, T+1):
+    testCaseNum = input()
+    checkChar = input()
+    data = input()
+
+    lengthOfCheckChar = len(checkChar)
+    lengthOfData = len(data)
+
+    cnt = 0
+    for i in range(lengthOfData):
+        if data[i:i+lengthOfCheckChar] == checkChar[:]:
+            cnt += 1
+
+    print("#{} {}".format(tc,cnt))
