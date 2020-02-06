401b0c03dcbda022ac915f5ca6c53cf81aac6a96
jade-min <smp1991@naver.com>
2020-02-06 21:01:26 +0900

    알고리즘 : SWEA D3 Python 4466번
    * 문제를 읽고 핵심을 파악할 수 있는가
    - 정렬을 이용하면 문제를 쉽게 풀 수 있다
    - 내장함수를 알고 있는 것과 내장함수만 아는 것은 다르다

---
+++ SWEA/D3/4466.최대 성적표 만들기_2nd.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('4466.최대 성적표 만들기_input.txt')
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+    scores = list(map(int, input().split()))
+
+    answer = 0
+    scores.sort(reverse=True)
+    print("#{} {}".format(tc, sum(scores[:K])))
